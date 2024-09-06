433ce0876ca37b1bc774bdbd99f62072489e4203
Vlad Semenuk <svg110308@gmail.com>
2024-09-06 22:27:22 +0300

    add levels

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ def home():
     return {"instructions": "go to /words"}
 
 
-@app.get("/words")
+@app.get("/a1_words")
 def get_a1_words():
     try:
         with open('words_a1.json', 'r', encoding='utf-8') as file:
@@ -25,14 +25,57 @@ def get_a1_words():
         print("JSONDecodeError")
 
 
+@app.get("/a2_words")
+def get_a1_words():
+    try:
+        with open('words_a2.json', 'r', encoding='utf-8') as file:
+            words = json.load(file)
+            print(words)
+        return words
+    except FileNotFoundError:
+        print("File not found")
+    except json.JSONDecodeError:
+        print("JSONDecodeError")
+
+
+@app.get("/b1_words")
+def get_a1_words():
+    try:
+        with open('words_b1.json', 'r', encoding='utf-8') as file:
+            words = json.load(file)
+            print(words)
+        return words
+    except FileNotFoundError:
+        print("File not found")
+    except json.JSONDecodeError:
+        print("JSONDecodeError")
 
 
 
 
+@app.get("/b2_words")
+def get_a1_words():
+    try:
+        with open('words_b2.json', 'r', encoding='utf-8') as file:
+            words = json.load(file)
+            print(words)
+        return words
+    except FileNotFoundError:
+        print("File not found")
+    except json.JSONDecodeError:
+        print("JSONDecodeError")
 
 
 
 
-
-
-
+@app.get("/c1_words")
+def get_a1_words():
+    try:
+        with open('words_c1.json', 'r', encoding='utf-8') as file:
+            words = json.load(file)
+            print(words)
+        return words
+    except FileNotFoundError:
+        print("File not found")
+    except json.JSONDecodeError:
+        print("JSONDecodeError")
