11ab5078bfc87e07e36db730c0fd7d608a1e5e10
Vlad Semenuk <svg110308@gmail.com>
2024-09-21 17:54:26 +0300

    fixed a little bug

--- fix.py
+++ fix.py
@@ -1,14 +1,17 @@
 import json
 
 # Load your JSON data
-with open("words_c1.json", 'r', encoding='utf-8') as f:
+with open("words_a1.json", 'r', encoding='utf-8') as f:
     data = json.load(f)
+
+for word in data['words']:
+    word["repetition"] = 0
 
 # Compress and preserve non-ASCII characters
 compressed_json = json.dumps(data, ensure_ascii=False, separators=(',', ':'))
 
 # Save it to a file
-with open("words_c11.json", 'w', encoding='utf-8') as f:
+with open("words_a11.json", 'w', encoding='utf-8') as f:
     f.write(compressed_json)
 
 print("JSON data compressed and saved successfully!")
